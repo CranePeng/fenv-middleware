5b8f25f0c9744df15598791baf0942905d8b9c2c
pengzihe <409716474@qq.com>
2022-02-19 16:18:15 +0800

    初始化中间件7

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"context"
+	"github.com/CranePeng/fenv-middleware/utils/common"
 	"github.com/CranePeng/fenv-middleware/utils/logger"
 	"testing"
 )
@@ -12,4 +13,5 @@ func TestLogDemo(t *testing.T) {
 	log.Info(conf.Ctx, "测试 %v", "asf")
 	log.Debug(conf.Ctx, "测试")
 	log.Warn(conf.Ctx, "测试")
+	common.GetCurrentAbPath()
 }
